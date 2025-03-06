eaeadde9b8d9b029834cbd862533e75fc6bf611c
Th3ndoo <lufunomakhado126@gmail.com>
2025-03-06 19:16:41 +0200

    Add files via upload

--- script1.js
+++ script1.js
@@ -1,29 +1,17 @@
-function validate(e){
-    e.preventdefault()
-    const email = document.getElementById("email").value 
-    const epass1 = document.getElementById("pass").value 
-    const age = document.getElementById("age").value 
-    const emsgbox = document.getElementById("message").value 
-    let message = ""
-    if (email == "")
+var index = 0
+function carousel()
+{
+    var i;
+    var x = document.getElementsByClassName("myslides")
+    for (i=0;i<x.length;i++)
     {
-        message = "Please enter the email"
-        msgbox.style.colour = "red"
+        x[i].style.display = "none";
     }
-    else if (pass1 === " ")
-    {
-        message = "Please enter the password"
-        msg.style.colour = "red"
+    index++;
+    if (index > x.length){
+        index = 1
     }
-    else if (age === "")
-    {
-        message = "Age must be between 12 and 50"
-        msg.style.colour = "red"
-    }
-    else {
-        message = "login successful"
-        msgbox.style.colour = "red"
-    }
-    
-    mesgbox.innerText+message;
+    x[index-1].style.display = "block";
+    setTimeout(carousel,3000)
 }
+carousel()
